7ef0eef46e89bb1b5ce0953839d2a6a63aeccbd2
David Hage <davhage@icloud.com>
2024-06-10 18:43:53 -0600

    corrigiendo errores

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const darkMode = document.body;
 button.addEventListener('click', function(){
     darkMode.classList.toggle('dark-mode');
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('menu');
+
+    menuToggle.addEventListener('click', () => {
+        menu.classList.toggle('active');
+    });
+});
